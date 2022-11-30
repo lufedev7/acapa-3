b0e1e2093f1a1c8fd28cacef87b9938b9e22fe3f
lufdev <luisfer738@gmail.com>
2022-11-29 21:58:25 -0500

    prueba backgrounnd tail

--- Components/SideBarMenuItems.js
+++ Components/SideBarMenuItems.js
@@ -1,6 +1,6 @@
 export default function SideBarMenuItems({ text, Icon, state }) {
   return (
-    <div className="">
+    <div className="bg-red-300">
       <Icon className="h-9 " />
       <span className={` ${state && "font-bold"} `}>{text}</span>
     </div>
